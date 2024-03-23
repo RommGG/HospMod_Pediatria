b204cd8328ac02d7e40717ca679dbdf87fc81368
RommGG <117605051+RommGG@users.noreply.github.com>
2024-03-22 22:12:07 -0600

    Delete Frontend/xray-main directory

--- Frontend/xray-main/react/src/store/setting/actions.js
+++
@@ -1,9 +0,0 @@
-import { settingSlice } from "./reducers";
-
-export const {
-  app_name,
-  theme_scheme_direction,
-
-  setSetting,
-} = settingSlice.actions;
-export default settingSlice.actions;
