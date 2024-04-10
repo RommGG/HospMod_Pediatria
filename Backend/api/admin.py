aa132251980f67a60492f8a82d477735fbeecd1c
Yair Tolentino <92339589+YahirMaster777@users.noreply.github.com>
2024-04-10 11:15:15 -0600

    Delete Backend/api directory

--- Backend/api/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import nacimientosBebes,SeguimientoPediatrico
-# Register your models here.
-admin.site.register(nacimientosBebes)
-admin.site.register(SeguimientoPediatrico)
