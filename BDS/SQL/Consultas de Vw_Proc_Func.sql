4e4af0df115a158d0de14dfa6add3f501e2aa372
EliAidan <155770097+EliAidan@users.noreply.github.com>
2024-04-22 16:06:08 -0600

    actualizacion de cambios en las consultas

--- BDS/SQL/Consultas de Vw_Proc_Func.sql
+++ BDS/SQL/Consultas de Vw_Proc_Func.sql
@@ -1,6 +1,6 @@
 /*Se manda a llamar el procedimiento que insertará a los bebés recien nacidos*/
 call sp_insertar_nacimientos_bebes(100);
-SELECT * FROM bd_hospital_210538.nacimiento_bebes;
+SELECT * FROM bd_hospital_210538.api_nacimientosbebes;
 /*Se mandan a llamar a las funciones*/
 SELECT fn_contar_nacimientos(); /*Cuenta a todos los bebés nacidos año por año*/
 SELECT fn_contar_nacimientos_por_tipo(); /*Cuenta a todos los bebés nacidos por Cesarea o Parto Natural*/
