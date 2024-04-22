a81eced69284ce7ec5c0b859ea556884178d11e5
Eli <210538@utxicotepec.edu.mx>
2024-04-21 18:10:50 -0600

    nuevas consultas agregadas en el archivo siendo consultas de las vistas

--- BDS/SQL/Consultas de Vw_Proc_Func.sql
+++ BDS/SQL/Consultas de Vw_Proc_Func.sql
@@ -6,5 +6,7 @@ SELECT fn_contar_nacimientos(); /*Cuenta a todos los bebés nacidos año por añ
 SELECT fn_contar_nacimientos_por_tipo(); /*Cuenta a todos los bebés nacidos por Cesarea o Parto Natural*/
 /*Se manda a llamar la vista que muestra la fecha de nacimiento, hora de nacimiento y el tipo de nacimiento*/
 SELECT * FROM vw_nacimientos;
+SELECT * FROM vw_nacimientos_genero;
+SELECT * FROM vw_nacimientos_por_ciudad;
 /*Limpia la base de datos completamente*/
 call sp_limpia_bd("qwerty");
